7272851df5629e62657a7e331385b2998277459a
DANIEL HAROLDO REYES GARCIA <dreyesg11@miumg.edu.gt>
2024-03-02 10:44:36 -0600

    Version 1.0

---
+++ src/main/java/com/mycompany/gasolinera/Gasolinera.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gasolinera;
+
+import com.mycompany.gasolinera.vista.Gasolinera_Servicio;
+
+/**
+ *
+ * @author 1214k
+ */
+public class Gasolinera {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Gasolinera_Servicio ventana = new Gasolinera_Servicio();
+        ventana.setVisible(true); 
+    }
+}
